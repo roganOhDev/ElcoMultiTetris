ef4dceafc16da2ffc63663b099297c15886edf64
roganOh <fdscbjdcnhd@gmail.com>
2023-04-19 22:25:12 +0900

    add todo

--- server/src/main/java/org/code/SendThead.java
+++ server/src/main/java/org/code/SendThead.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class SendThead extends Thread {
+public class SendThead extends Thread { // TODO : send 는 쓰레드가 아니어도 되지 않을까?
 
     private final Socket socket;
     private Scanner scanner = new Scanner(System.in);
