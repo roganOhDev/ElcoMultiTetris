5031c2e3aa56368d72fe12d6be8bc17feff71ce4
roganOh <fdscbjdcnhd@gmail.com>
2023-04-25 13:01:54 +0900

    reformat

--- server/src/main/java/org/code/ReceiveThread.java
+++ server/src/main/java/org/code/ReceiveThread.java
@@ -3,7 +3,6 @@ package org.code;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
-import java.net.SocketException;
 
 import static org.code.Log.log;
 
@@ -29,18 +28,24 @@ public class ReceiveThread extends Thread {
                 }
 
             }
-        } catch (SocketException e1) {
+        } catch (IOException e1) {
             log.error("Error While Receving Message : ");
 
-//            utils.removeClient(Main.clients, Main.messageSenders, 1);
-        } catch (IOException e2) {
-            e2.printStackTrace();
+            findSocketAndDelete();
         }
     }
 
-    private String receivedData(byte[] buffer, int numBytes){
+    private void findSocketAndDelete() {
+        for (int i = 0; i < Main.clients.size(); i++) {
+            if (Main.clients.get(i).getClientSocket() == socket) {
+                utils.removeClient(Main.clients, Main.messageSenders, i);
+            }
+        }
+    }
+
+    private String receivedData(byte[] buffer, int numBytes) {
         final var receivedData = new String(buffer, 0, numBytes);
-        log.info("Received data from "+ socket.getInetAddress() + " : " + socket.getPort() + " : " + receivedData);
+        log.info("Received data from " + socket.getInetAddress() + " : " + socket.getPort() + " : " + receivedData);
 
         return receivedData;
     }
