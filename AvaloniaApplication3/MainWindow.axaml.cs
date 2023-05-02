2b039bfbc1b0119cf3e4f84bf73e77bc4d57cbc0
bacordgit <bacord15@naver.com>
2023-05-02 20:17:23 +0900

    message

---
+++ AvaloniaApplication3/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaApplication3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
